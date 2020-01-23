bcf0d77bf74378a27eebe08825b94ec9418d3cff
Lomakin Yevhen <jeklom@gmail.com>
2020-01-23 17:30:23 +0300

    small fix

--- weblab8/backend/routes/comments.js
+++ weblab8/backend/routes/comments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const articleRepo = require('./../db/database');
 
-router.get('/add', async function (req, res, next) {
+router.post('/add', async function (req, res, next) {
     const {postId, user, date, text} = req.query;
 
     const bla = await articleRepo.addComment(postId, user, date, text);
